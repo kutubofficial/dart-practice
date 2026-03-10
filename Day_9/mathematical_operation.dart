3360080fc88cdc230e17416387825ca0daee3c72
Kutubuddin Ansari <kutubofficial599@gmail.com>
2026-03-10 20:26:26 +0530

    mathematical calculation using switch case

--- Day_9/mathematical_operation.dart
+++ Day_9/mathematical_operation.dart
@@ -1,4 +1,31 @@
+import 'dart:io';
+
 void main() {
+  stdout.write('Enter you first number ');
+  int num1 = int.parse(stdin.readLineSync().toString());
+  stdout.write('Enter you second number ');
+  int num2 = int.parse(stdin.readLineSync().toString());
+  stdout.write('Choose Option - (1-Add, 2-Subtract, 3-Multiply, 4-Divide)- ');
+  int options = int.parse(stdin.readLineSync().toString());
+  switch (options) {
+    case 1:
+      print('Result: ${num1 + num2}');
+      break;
+    case 2:
+      print('Result: ${num1 - num2}');
+      break;
+    case 3:
+      print('Result: ${num1 * num2}');
+      break;
+    case 4:
+      print('Result: ${num1 / num2}');
+      break;
+    default:
+      print('Invalid Option');
+  }
+}
+
+dynamic singleLinerCalculation() {
   print(add(10, 20));
   print(subtract(80, 20));
   print(divide(m: 10, n: 2));
