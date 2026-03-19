bb95a290845b8caef0594c1d817d0559e59040a0
Kutubuddin Ansari <kutubofficial599@gmail.com>
2026-03-19 18:45:30 +0530

    added some basic question

---
+++ Day_11/learn.dart
@@ -0,0 +1,20 @@
+//! REST API
+// Rest API stands for Representational State Transfer Application Programming Interface.
+// It is a set of rules and conventions for building and interacting with web services.
+// - like it allows us to perform CRUD operations. 
+// It gives client communicates with server using HTTP methods like GET, POST, PUT, DELETE etc.
+
+
+//! FLUTTER - 
+
+//How many third-party libraries have you used?
+// I have used several third-party libraries depending on the project requirements.
+
+//~ What is Flutter? 
+// Flutter is an open-source UI toolkit developed by Google that allows developers to build cross-platform applications for Android, iOS, web, and desktop using a single codebase.
+
+//~ What is Dart?
+// Dart is the programming language used to develop Flutter applications. It is optimized for building fast UI applications and supports both JIT and AOT compilation.
+
+//~ What are Widgets in Flutter?
+// Widgets are the basic building blocks of a Flutter UI. Everything in Flutter is a widget, such as text, buttons, layouts, and images.
