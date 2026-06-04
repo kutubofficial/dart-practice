d8c3dcc80ac5b0875249400dbe8c3dcc85983337
Kutubuddin Ansari <kutubofficial599@gmail.com>
2026-06-04 15:18:49 +0530

    just practicing myself

---
+++ Practice/index.dart
@@ -0,0 +1,27 @@
+//!INDRODUCTION - 
+/**
+ * My name is Kutubuddin Ansari and I live in Noida Sector 15. I have completed 
+ * my MCA and worked as Fluttet Developer at Webninjaz Pvt. Ltd in Noida-59.
+ * I have good knowledge of Flutter, Dart and along with Web Technologies such as
+ * a HTML,CSS,JavaScript and ReactJS.
+ * I have worked on Project like the Pacific Hostel Management System and PreQt mobile App 
+ * Restaurant Management System. and I am pasinated in mobile App Development, 
+ * apart from that I enjoy playing chess.
+ */
+
+//! Reason of resignation
+
+/**
+ * The main reasons were my MCA final-year exams and some important family commitments,
+ *  including my brother's wedding. Additionally,
+ *  my company was relocating its office farther from my residence,
+ *  which would have made the commute difficult. After considering these factors,
+ *  I decided to resign and focus on completing my academic commitments.
+ */
+
+//~ I resign the company because I had to my MCA exam and I had to go to my Hometown 
+//~ for my brother wedding at the same time and additionally company was relocation the office 
+//~ and it could be more far from my resident like reaching daily to office taking exta time.
+
+// I play chess because it improve the logical thinking, patience and decison making and 
+// i think it help me in programming because both required problem-solving.
