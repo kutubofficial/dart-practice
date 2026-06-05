a8ef9ab3666f688963966eef8966aa370511ded1
Kutubuddin Ansari <kutubofficial599@gmail.com>
2026-06-05 16:14:24 +0530

    revise

---
+++ Practice/flutter.dart
@@ -0,0 +1,14 @@
+//Flutter- Flutter is an open-source UI framework developed by Google.
+//it allow us to build cross-plateform applications for Android, iOS, web and desktop.
+
+//it is a single code-base 
+
+//* Widgets - in Flutter everything is widgets, widgets are block of code and it decide how 
+//* UI look and his behaviour.
+
+//~ Statefull - immutable
+//~ stateless - mutable 
+
+//& - Hot Reload and Hot Restart
+//- basically if we need to see the changes instant we use hot reload and 
+// if we use Hot Restart it destroy the build 
