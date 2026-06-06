becd9dd392f7e6819c6bf0d3b4af1c3a9ed64e3a
Kutubuddin Ansari <kutubofficial599@gmail.com>
2026-06-06 14:30:15 +0530

    revie about project and packages

---
+++ About_Projects/about.dart
@@ -0,0 +1,14 @@
+//!Confidence doesn't come from speaking loudly. It comes from knowing, "I actually worked on this project, so I can explain it."
+
+//~ PMCH (Pacific Hostel Management System)
+//PMCH is a hostel managament application design to digitize hostel oprations.
+//It helps to manage room allocation, complaints, gate-pass and student attendance.
+
+//---PURPOSE
+//The purpose is to reduce the papaerwork and make hostel work more efficient.
+
+//~PreQT App
+//PreQT is a Finance Tech application and it allow users to explore pre-IPO and private market investment.
+
+//---PURPOSE
+//This application provides investment-related informations and gives opportunity to invest in private market.
